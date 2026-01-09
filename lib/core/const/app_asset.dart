d08b5ef90aceeba0cc669d8cd1cfe617874f4420
NadeenRR <nadeenradwan0@gmail.com>
2026-01-09 13:44:35 +0200

    track order screen done

--- lib/core/const/app_asset.dart
+++ lib/core/const/app_asset.dart
@@ -1,3 +1,8 @@
 class AppAsset {
   static const String completeOrder = "assets/images/complete.png";
+  static const String takeOrder = "assets/images/take_order.png";
+  static const String prepared = "assets/images/prepared.png";
+  static const String delivery = "assets/images/delivery.png";
+  static const String done = "assets/images/done.png";
+  static const String map = "assets/images/map.png";
 }
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,7 +1,11 @@
-
 import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFFFFAB61);
   static const Color darkPurple = Color(0xFF27214D);
+  static const Color lightYellow = Color(0xffFFFAEB);
+  static const Color lightGrey = Color(0xffF1EFF6);
+  static const Color lightPink = Color(0xffFEF0F0);
+  static const Color lightGreen = Color(0xffF0FEF8);
+  static const Color lightOrange = Color(0xFFFFE3C9);
 }
